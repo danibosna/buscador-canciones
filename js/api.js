601faf1c954405e945ab975618e6cd49799c371c
danibosna <daniel.d.boscan@gmail.com>
2022-06-17 13:30:50 -0500

    [INIT]

---
+++ js/api.js
@@ -0,0 +1,35 @@
+import * as UI from "./interfaz.js";
+
+class API {
+  constructor(artista, cancion) {
+    this.artista = artista;
+    this.cancion = cancion;
+
+    this.consultarAPI();
+  }
+
+  consultarAPI() {
+    const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`;
+
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        if (UI.divResultado.lyrics) {
+          const { lyrics } = datos;
+          UI.divResultado.textContent = lyrics;
+          UI.headingResultado.textContent = `Letra de la cancion: ${this.cancion} de ${this.artista}`;
+        } else {
+          UI.divMensaje.textContent =
+            "La cancion no existe prueba con otra cancion";
+          UI.divMensaje.classList.add("error");
+
+          setTimeout(() => {
+            UI.divMensaje.textContent = "";
+            UI.divMensaje.classList.remove("error");
+          }, 3000);
+        }
+      });
+  }
+}
+
+export default API;
---
+++ js/app.js
@@ -0,0 +1,28 @@
+import * as UI from "./interfaz.js";
+import API from "./api.js";
+
+UI.formularioBuscar.addEventListener("submit", buscarCancion);
+
+function buscarCancion(e) {
+  e.preventDefault();
+
+  //obtener datos del formulario
+  const artista = document.querySelector("#artista").value;
+  const cancion = document.querySelector("#cancion").value;
+
+  //validar el formulario
+  if ((artista === "", cancion === "")) {
+    UI.divMensaje.textContent = "Error... Todos los campos son obligatorios";
+    UI.divMensaje.classList.add("error");
+
+    setTimeout(() => {
+      UI.divMensaje.textContent = "";
+      UI.divMensaje.classList.remove("error");
+    }, 3000);
+
+    return;
+  }
+
+  //consultar API
+  const busqueda = new API(artista, cancion);
+}
